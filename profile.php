8d8d923f6285de2f31624995a49122fe99dcdb95
Hau Luu <hatrluu@gmail.com>
2018-02-13 00:27:26 -0500

    Testing jQuery

--- profile.php
+++ profile.php
@@ -3,8 +3,22 @@
 <head>
 <meta charset="utf-8">
 <title>User Profile</title>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
 </head>
 <body>
+	<script>
+		$(function(){
+				$('#button').on('click',function(){
+					$('#data').toggle();
+					$('#edit-field').toggle();
+				});
+		});
+	</script>
+	<button onclick="window.location.href='//localhost/HealthWebApp/'"> Go Back </button>
 		<?php
             $xmlFileName = "xml/userprofile.xml";
             $users = simplexml_load_file($xmlFileName);
@@ -16,19 +30,38 @@
             }
 
             else{
-                echo "Here are your data: <br>";
                 //For future with multiple user support
                 foreach($users as $user){
-                    echo "Name: ". $user->fname. " ". $user->lname ."<br>";
-                    echo "Gender: ". $user->genders. "<br>";
-                    echo "Date of Birth: ". $user->dob. "<br>";
-                    echo "Height: ". $user->heightfeet." ft.". $user->heightinch. " in.<br>";
-                    echo "Weight: ". $user->weight. " lbs<br>";
+									$fname = $user->fname;
+									$lname = $user->lname;
+									$genders = $user->genders;
+									$dob = $user->dob;
+									$heightfeet = $user->heightfeet;
+									$heightinch = $user->heightinch;
+									$weight = $user->weight;
                 }
             }
 		?>
-        <input type="button" value="Edit">
-        <button onclick="window.location.href='//localhost/HealthWebApp/'"> Go Back </button>
+				<div id="data">
+					<?php
+						echo "Name: ". $fname. " ". $lname ."<br>";
+						echo "Gender: ". $genders. "<br>";
+						echo "Date of Birth: ". $dob. "<br>";
+						echo "Height: ". $heightfeet." ft.". $heightinch. " in.<br>";
+						echo "Weight: ". $weight. " lbs<br>";
+					?>
+				</div>
+				<div id="edit-field" hidden>
+					First name: <input type="text" name="fname" value=<?php echo $fname;?>><br />
+					Last name: <input type="text" name="lname" value=<?php echo $lname;?>><br />
+					Gender: <input type="text" name="genders" value=<?php echo $genders;?>><br />
+					Date-of-birth<input type="text" name="dob" value=<?php echo $dob;?>><br />
+					Height: <input type="text" name="heightfeet" value=<?php echo $heightfeet;?>> ft.
+					<input type="text" name="heightinch" value=<?php echo $heightinch;?>> in.<br />
+					Weight <input type="text" name="weight" value=<?php echo $weight;?>> lbs<br />
+				</div>
+				<input type="button" value="Edit">
+
 
 </body>
 </html>
